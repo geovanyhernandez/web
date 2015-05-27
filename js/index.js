380e54050f34fa44df02cf3bcd1929dde790fd3c
giovanyhernandez <giovanyhernandez@gmail.com>
2015-05-27 08:44:26 +0200

    terminado

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $('#formulario').submit(function() { 
 	
 	 
-	// recolecta los valores que inserto el usuario
+	// recolecta los datos  insertados por el usuario
 	var datosUsuario = $("#nombredeusuario").val()
 	var datosPassword = $("#clave").val()
 	
-  	//archivoValidacion = "http://192.168.0.213/index.php?jsoncallback=?"
+  	
   	archivoValidacion = "https://enigmatic-ravine-8583.herokuapp.com?jsoncallback=?"
 
 	$.getJSON( archivoValidacion, { usuario:datosUsuario ,password:datosPassword})
@@ -15,7 +15,7 @@ $('#formulario').submit(function() {
 		
 		if(respuestaServer.validacion == "ok"){
 		  
-		 	/// si la validacion es correcta, muestra la pantalla "home"
+		 	/// si la validacion es correcta, muestra la pantalla "home" y si no es correctas nos mostrara error.
 			$.mobile.changePage("#home")
 		  
 		}else{
